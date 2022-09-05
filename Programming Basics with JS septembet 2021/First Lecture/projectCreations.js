8fb41234224146c4e41662ad0223a484109cc183
AleksandarVulchanov <aleks.vulchanov@abv.bg>
2022-09-05 15:45:11 +0300

    Delete Programming Basics with JS septembet 2021/First Lecture directory

--- Programming Basics with JS septembet 2021/First Lecture/projectCreations.js
+++
@@ -1,9 +0,0 @@
-function projectCreations(input) {
-    let name = input[0];
-    let numberProject = Number(input[1])
-    let hours = numberProject * 3
-
-        console.log(`The architect ${name} will need ${hours} hours to complete ${numberProject} project/s.`);
-}
-
-projectCreations(["George ","4 "]);
